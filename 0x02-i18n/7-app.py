fbc0e5814d5e79688d7d8dd7d76ce0042d4f0ba2
jeff_makuto <68159268+jeffmakuto@users.noreply.github.com>
2024-04-12 17:37:52 +0300

    Update 7-app.py

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -20,10 +20,14 @@ app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+    1: {"name": "Balou", "locale": "fr",
+        "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en",
+        "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg",
+        "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None,
+        "timezone": "Europe/London"},
 }
 
 
